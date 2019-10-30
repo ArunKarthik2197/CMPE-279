00ef8613cb3d918596c1464d8e01131866f20fb8
ArunKarthik2197 <arun.munipalle1997@gmail.com>
2019-10-29 23:15:47 -0700

    minor changes in reader

--- assignment2/reader.c
+++ assignment2/reader.c
@@ -12,10 +12,15 @@ int main(int argc, char const *argv[])
 	//setuid(65534); 
 	/*user - nobody(uid - 65534), current_user(uid -1000)
 	use id command to get the userIds	*/
-        int new_socket=argv[1][0];
-	const char* hello=argv[2];
-        char buffer[1024]={0};
-    	int valread = read( new_socket , buffer, 1024); 
+	int new_socket;
+	const char* hello;
+	if(argc > 0){
+        new_socket=*argv[1];
+	hello=argv[2];
+	}
+	char buffer[1024] = {0};
+	printf("debug: %s\n\n",argv[3]);
+    	int valread = read( new_socket ,buffer, 1024); 
     	printf("%s\n",buffer ); 
     	send(new_socket , hello , strlen(hello) , 0 ); 
     	printf("Hello message sent\n"); 
