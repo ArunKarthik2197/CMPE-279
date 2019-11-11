92656a01805041a9ebc19457a489989e5ad8fed8
karthika <kartiinx@gmail.com>
2019-11-11 13:15:34 -0800

    fork & exec in same binary

--- assignment3/server.c
+++ assignment3/server.c
@@ -1,4 +1,5 @@
 // Server side C/C++ program to demonstrate Socket programming 
+#define _GNU_SOURCE
 #include <unistd.h> 
 #include <stdio.h> 
 #include <sys/socket.h> 
@@ -7,11 +8,13 @@
 #include <string.h> 
 #include <sys/wait.h>
 #include <fcntl.h>
+#include <sys/stat.h>
 #define RED(string) "\x1b[31m" string "\x1b[0m"
 #define BLUE(string) "\x1b[34m" string "\x1b[0m"
 
 //#define PORT 8080 
-int main(int argc, char const *argv[]) 
+int fileSize(int fd);
+int main(int argc, char const *argv[], char const *env[]) 
 { 
     int server_fd, new_socket, valread; 
     struct sockaddr_in address; 
@@ -25,6 +28,8 @@ int main(int argc, char const *argv[])
     pid_t parent_pid;   
     pid_t current_pid; 
     current_pid=getpid();
+     char *envp = getenv("CHECK");
+     if(envp == NULL || *envp == '\0'){
     if(argc>2)
     {
 	port = strtol(argv[1],NULL,10);
@@ -116,7 +121,11 @@ int main(int argc, char const *argv[])
     {
 	if(file_fd>0)
 	{
-	if(execl("reader","reader",&new_socket,&file_fd,NULL)<0)
+		//setenv("CHECK","child",1);
+	
+	char *env[] ={"CHECK=parent",NULL};
+	printf("execle - ing to child \n");
+	if(execle("server","server",&new_socket,&file_fd,(char *) NULL,env)<0)
 	{
 		printf("exec error\n");
 		exit(0);
@@ -146,7 +155,108 @@ int main(int argc, char const *argv[])
     else
     {
        printf(RED("Invalid Port number\n"));  
-    }
+    }}
+    else
+   {
+    printf("Its working");
+    
+	printf("Child created...reading from client\n");
+	char cwd[100];
+	int chr,new_socket,file,bytes_read,file_size;
+	char *hello; 
+	// getting current working directory
+	if(getcwd(cwd,sizeof(cwd))!=NULL)
+        {
+                printf("working directory before chroot: %s\n",cwd);
+		// appending the empty directory name to the path
+                strncat(cwd,"/emptyDir",sizeof(cwd)-strlen(cwd)-1);
+		printf("directory after appending: %s\n",cwd);
+        }
+        else
+        {
+                printf("getcwd error before chroot\n");
+        }
+
+	//changing directory to the fake root
+        if(chdir(cwd)==0){
+	printf("chdir success \n");
+        chr = chroot(cwd); // making the present empty directory a fake root
+        }
+        else
+        {
+        printf("chdir error...\n");
+	exit(0);
+        }
+
+	// check for command line args
+	if(argc > 1){
+        new_socket=*argv[1];
+	file  = *argv[2];
+	}
+	else // for command line args check
+	{
+	printf("Error...\n");
+	exit(0);
+	}
+
+	file_size = fileSize(file);
+	printf("size of the file: %d\n",file_size);
+	char buffer[1024] = {0};
+	if(file_size>-1)
+	{
+	hello = (char*)calloc(file_size,sizeof(char));
+	}
+	
+	// checking if the chroot succeeded
+	if(chr == 0){
+	// testting the chroot
+	if(getcwd(cwd,sizeof(cwd))!=NULL)
+        {
+                printf(RED("working directory after chroot: %s\n"),cwd);
+        }
+        else
+        {
+                printf("getcwd error after chroot\n");
+        }
+	
+	// dropping privilege
+	if(setuid(65534)== 0 ){
+    	int valread = read( new_socket ,buffer, 1024); 
+    	printf("%s\n",buffer );
+	//converting file contents to string
+	if(file >0)
+	{
+		bytes_read = read(file,hello,file_size);
+		if(bytes_read>0)
+		{
+			hello[bytes_read] = '\0';
+		}
+	}
+	else // for file
+	{
+		printf("Empty File\n");
+	}
+	// sending the file_size
+	file_size= htons(file_size);
+ 	write(new_socket, &file_size,sizeof(int));
+	// sending the contents of actual file	
+    	send(new_socket , hello , strlen(hello) , 0 ); 
+	
+    	printf("Hello message sent\n"); 
+	printf("IN CHILD ->current_user_id: %d\n",getuid());
+	}
+	else // for setuid
+	{
+		printf("access denied");
+	}
+	}
+	else // for chroot
+	{
+		printf("chroot error\n");
+	}
+	free(hello);	
+	return 0;
+   }
     return 0; 
 }
 /*char* fileType(int fd) {
@@ -156,3 +266,11 @@ int main(int argc, char const *argv[])
    }
    return(s.st_mode);
 } */
+
+int fileSize(int fd) {
+   struct stat s;
+   if (fstat(fd, &s) == -1) {
+      return(-1);
+   }
+   return(s.st_size);
+}
